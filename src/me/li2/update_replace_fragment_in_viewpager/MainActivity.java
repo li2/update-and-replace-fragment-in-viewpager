acce5448799cc1596e315054a2469f627b9a0330
weiyi.li <weiyi.just2@gmail.com>
2015-08-07 13:45:02 +0800

    Init project.

---
+++ src/me/li2/update_replace_fragment_in_viewpager/MainActivity.java
@@ -0,0 +1,14 @@
+package me.li2.update_replace_fragment_in_viewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
